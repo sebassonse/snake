0327dc76f4ff25df64730d89708dad553b911417
sebassonse <sascha.bessonova@yandex.ru>
2021-09-14 17:22:05 +0300

    в experiments_without_classes.py лежит рабочая и дооформленная

--- experiments/experiments_without_classes.py
+++ experiments/experiments_without_classes.py
@@ -35,7 +35,39 @@ while [appleX_init, appleY_init] == [snakeX_init, snakeY_init]:
     appleY_init = random.randrange(0, Screen_size, apple_size)
 
 snake_placement_list = [tuple([snake_head_rect.x, snake_head_rect.y])]
-# classes and other
+
+# classes and functions
+
+
+def check_pressed_keys():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and not speed[0] == snake_speed:
+        speed[0] = -snake_speed
+        speed[1] = 0
+        keys = []
+    elif keys[pygame.K_RIGHT] and not speed[0] == -snake_speed:
+        speed[0] = snake_speed
+        speed[1] = 0
+        keys = []
+    elif keys[pygame.K_UP] and not speed[1] == snake_speed:
+        speed[0] = 0
+        speed[1] = -snake_speed
+        keys = []
+    elif keys[pygame.K_DOWN] and not speed[1] == -snake_speed:
+        speed[0] = 0
+        speed[1] = snake_speed
+        keys = []
+
+
+def field_looping(snake_head_rect):
+    if snake_head_rect.top < 0:
+        snake_head_rect.bottom = Height
+    elif snake_head_rect.bottom > Height:
+        snake_head_rect.top = 0
+    elif snake_head_rect.right > Width:
+        snake_head_rect.left = 0
+    elif snake_head_rect.left < 0:
+        snake_head_rect.right = Width
 
 
 # initialisation pygame
@@ -64,39 +96,20 @@ while not game_over:
 
     # ДВИЖЕНИЕ
     # нажатые клавиши
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and not speed[0] == snake_speed:
-        speed[0] = -snake_speed
-        speed[1] = 0
-    if keys[pygame.K_RIGHT] and not speed[0] == -snake_speed:
-        speed[0] = snake_speed
-        speed[1] = 0
-    if keys[pygame.K_UP] and not speed[1] == snake_speed:
-        speed[0] = 0
-        speed[1] = -snake_speed
-    if keys[pygame.K_DOWN] and not speed[1] == -snake_speed:
-        speed[0] = 0
-        speed[1] = snake_speed
+    check_pressed_keys()
 
     # движение змейки
+
     snake_head_rect.x += speed[0]
     snake_head_rect.y += speed[1]
 
     # зацикленность игрового поля
-    if snake_head_rect.top < 0:
-        snake_head_rect.bottom = Height
-    elif snake_head_rect.bottom > Height:
-        snake_head_rect.top = 0
-    elif snake_head_rect.right > Width:
-        snake_head_rect.left = 0
-    elif snake_head_rect.left < 0:
-        snake_head_rect.right = Width
+    field_looping(snake_head_rect)
 
     # ПОЕДАНИЕ И РОСТ
     # поедание
     if [snake_head_rect.x, snake_head_rect.y] == [appleX_init, appleY_init]:
         snake_placement_list.append(tuple([0, 0]))
-
         appleX_init = random.randrange(0, Screen_size, apple_size)
         appleY_init = random.randrange(0, Screen_size, apple_size)
 
@@ -113,6 +126,7 @@ while not game_over:
     if len(snake_placement_list) > 1:
         for i in range(len(snake_placement_list)):
             pygame.draw.rect(Screen, GREEN, [snake_placement_list[i][0], snake_placement_list[i][1], snake_size, snake_size])
+
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
